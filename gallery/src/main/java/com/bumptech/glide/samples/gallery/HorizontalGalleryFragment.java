0ed47706b844de8ad351375e572929ed8b2e1870
gaoxiaohui <1107875131@qq.com>
2017-10-31 11:21:41 +0800

    small changes;

--- gallery/src/main/java/com/bumptech/glide/samples/gallery/HorizontalGalleryFragment.java
+++ gallery/src/main/java/com/bumptech/glide/samples/gallery/HorizontalGalleryFragment.java
@@ -4,16 +4,22 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.bumptech.glide.integration.recyclerview.RecyclerViewPreloader;
 import com.smart.adapter.recyclerview.ViewHolder;
+import com.smart.view.decoration.DividerGridItemDecoration;
 import com.smart.view.decoration.DividerItemDecoration;
 
 import java.util.List;
@@ -26,10 +32,15 @@ public class HorizontalGalleryFragment extends Fragment
 
     private RecyclerView recyclerView;
 
+    DividerGridItemDecoration itemDecoration_stagger;
+    DividerItemDecoration itemDecoration_linear_grid;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getLoaderManager().initLoader(R.id.loader_id_media_store_data, null, this);
+
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -38,14 +49,17 @@ public class HorizontalGalleryFragment extends Fragment
         View result = inflater.inflate(R.layout.recycler_view, container, false);
         recyclerView = (RecyclerView) result.findViewById(R.id.recycler_view);
 
+        // 分割线
+        itemDecoration_stagger = new DividerGridItemDecoration(this.getContext());
+        itemDecoration_linear_grid = new DividerItemDecoration(this.getContext(),
+                DividerItemDecoration.VERTICAL);
+
         // use a linear layout manager
         LinearLayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
         mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(mLayoutManager);
 
-        DividerItemDecoration itemDecoration = new DividerItemDecoration(getActivity(),
-                DividerItemDecoration.VERTICAL);
-        recyclerView.addItemDecoration(itemDecoration);
+        recyclerView.addItemDecoration(itemDecoration_linear_grid);
 
         return result;
     }
@@ -61,8 +75,7 @@ public class HorizontalGalleryFragment extends Fragment
         Log.d("Loader fragment", "onLoadFinished called!");
 
         RecyclerCommonAdapter<MediaStoreData> mAdapter = new RecyclerCommonAdapter<MediaStoreData>(
-                getActivity(), R.layout.layout_list_item_video,
-                mediaStoreDataList) {
+                getActivity(), R.layout.layout_list_item_video, mediaStoreDataList) {
 
             @Override
             protected void convert(final ViewHolder holder, MediaStoreData data) {
@@ -82,5 +95,86 @@ public class HorizontalGalleryFragment extends Fragment
     @Override
     public void onLoaderReset(Loader<List<MediaStoreData>> loader) {
         // Do nothing.
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.menu_pull_up_down, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+        // 获取当前第一个可见Item的position
+        int scrollPosition = 0;
+        // If a layout manager has already been set, get current scroll position.
+        if (layoutManager != null) {
+            if (layoutManager instanceof StaggeredGridLayoutManager) {
+                scrollPosition = ((StaggeredGridLayoutManager) layoutManager)
+                        .findFirstCompletelyVisibleItemPositions(null)[0];
+            } else {
+                scrollPosition = ((LinearLayoutManager) layoutManager)
+                        .findFirstCompletelyVisibleItemPosition();
+            }
+        }
+
+        if (id == R.id.action_layout_linear) {
+
+            if (layoutManager instanceof GridLayoutManager ||
+                    !(layoutManager instanceof LinearLayoutManager)) {
+                // use a linear layout manager
+                LinearLayoutManager mLayoutManager = new LinearLayoutManager(this.getContext());
+                mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                recyclerView.setLayoutManager(mLayoutManager);
+
+                // 分割线处理
+                recyclerView.removeItemDecoration(itemDecoration_stagger);
+                recyclerView.addItemDecoration(itemDecoration_linear_grid);
+            }
+            // scroll to position
+            recyclerView.scrollToPosition(scrollPosition);
+
+            return true;
+        } else if (id == R.id.action_layout_grid) {
+
+            if (!(layoutManager instanceof GridLayoutManager)) {
+                // use a grid layout manager
+                GridLayoutManager mGridLayoutManager = new GridLayoutManager(this.getContext(), 3);
+                mGridLayoutManager.setOrientation(GridLayoutManager.VERTICAL);
+                recyclerView.setLayoutManager(mGridLayoutManager);
+
+                // 分割线处理
+                recyclerView.addItemDecoration(itemDecoration_stagger);
+                recyclerView.removeItemDecoration(itemDecoration_linear_grid);
+            }
+            // scroll to position
+            recyclerView.scrollToPosition(scrollPosition);
+
+            return true;
+        }
+        //        else if (id == R.id.action_layout_stagger) {
+        //            if (!(layoutManager instanceof StaggeredGridLayoutManager)) {
+        //                // use a s layout manager
+        //                StaggeredGridLayoutManager mStaggeredLayoutManager =
+        //                        new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        //                recyclerView.setLayoutManager(mStaggeredLayoutManager);
+        //
+        //                // 分割线处理
+        //                recyclerView.removeItemDecoration(itemDecoration_linear_grid);
+        //                recyclerView.addItemDecoration(itemDecoration_stagger);
+        //            }
+        //
+        //            // scroll to position
+        //            //recyclerView.scrollToPosition(scrollPosition);
+        //
+        //            return true;
+        //        }
+
+        return super.onOptionsItemSelected(item);
+
     }
 }
